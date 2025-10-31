d673c2042c1eaa296c4bb8ea4b9f861b9a87e8c0
Tung Nguyen X. <xuntgn12@gmail.com>
2025-10-31 18:33:05 +0700

    October 31st 2025 - fix minor import error

--- backend/src/main/java/socialmediaspringboot/backend/service/User/UserServiceImpl.java
+++ backend/src/main/java/socialmediaspringboot/backend/service/User/UserServiceImpl.java
@@ -1,6 +1,5 @@
 package socialmediaspringboot.backend.service.User;
 
-import lombok.var;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContextException;
 import org.springframework.dao.DataIntegrityViolationException;
