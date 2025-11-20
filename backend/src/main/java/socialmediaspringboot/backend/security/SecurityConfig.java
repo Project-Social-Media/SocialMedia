3b1eb20b31f290ea0724d12f63a2044ce97ae76e
BuiTrongThanh03 <thanhbuitrong03@gmail.com>
2025-11-20 12:26:32 +0700

    authorization

--- backend/src/main/java/socialmediaspringboot/backend/security/SecurityConfig.java
+++ backend/src/main/java/socialmediaspringboot/backend/security/SecurityConfig.java
@@ -13,7 +13,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 import socialmediaspringboot.backend.service.User.MyUserDetailsService;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -29,9 +32,17 @@ public class SecurityConfig {
         httpSecurity
                 .csrf(csrf -> csrf.disable()) // ko dung session -> disable CSRF
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // khong luu session
+                .cors(cors -> cors.configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.setAllowedOrigins(List.of("http://localhost:3000")); // url fe
+                    config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+                    config.setAllowedHeaders(List.of("*"));
+                    config.setAllowCredentials(true);
+                    return config;
+                }))
                 .authorizeHttpRequests(auth -> auth  // phan quyen
                         .requestMatchers("/api/auth/**").permitAll() // cho signup, login
-                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/admin/**").hasRole("ROLE_ADMIN")
                         .requestMatchers("/api/users/create").permitAll()
                         .requestMatchers("/api/users/**").authenticated()
                         .anyRequest().permitAll()
