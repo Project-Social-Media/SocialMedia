c0ce55377322e8dc171d7b66f17f094cd641d891
Tung Nguyen X. <xuntgn12@gmail.com>
2025-12-17 22:24:23 +0700

    December 17th 2025: - modify media service

--- backend/src/main/java/socialmediaspringboot/backend/service/Media/MediaService.java
+++ backend/src/main/java/socialmediaspringboot/backend/service/Media/MediaService.java
@@ -5,6 +5,10 @@ import socialmediaspringboot.backend.dto.Media.MediaRequestDTO;
 import socialmediaspringboot.backend.dto.Media.MediaResponseDTO;
 import socialmediaspringboot.backend.model.Media;
 
+import java.util.List;
+
 public interface MediaService {
     public MediaResponseDTO upload(MultipartFile file, MediaRequestDTO request);
+    public MediaResponseDTO getMediaById(Long mediaId);
+    public void deleteMedia(List<Long> mediaId);
 }
